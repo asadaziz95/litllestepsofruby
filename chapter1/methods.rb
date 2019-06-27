421ab90452a44a2c3a8caef516e80fae67d084df
asadazizrajput <asadazizjs@gmail.com>
2019-06-27 10:41:36 +0500

    refactorign code

--- chapter1/methods.rb
+++ chapter1/methods.rb
@@ -6,16 +6,8 @@
 # saysomething
 
 def get2number(afirstname, asecondname)
-  d = "Helo #{afirstname} first #{asecondname}"
-  e = "Helo #{afirstname} last #{asecondname}"
-  f = "Helo #{afirstname} lastone #{asecondname}"
-  a = 12
-  b = 13
-  c = 34
-  g = d + e + f
-  a + b + c
-  g
+  "Hello #{afirstname}  #{asecondname}"
 end
 
-result = get2number(15, 20)
+result = get2number(asad, aziz)
 puts "#{result} is the result"
